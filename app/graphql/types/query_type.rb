bb76f62a864da803ee3e67b5f77fd3a39a4af609
Felipe Gentil <cdigentil@gmail.com>
2020-06-22 09:52:20 -0300

    poc step 0

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,4 +1,15 @@
 module Types
   class QueryType < Types::BaseObject
+    GraphqlPoc::QueryReader.new.content[:queries].each do |query|
+      method = query.keys.first
+      type = query[method][:type]
+      value = query[method][:value]
+
+      field method, type, null: false
+
+      define_method method do
+        value
+      end
+    end
   end
 end
